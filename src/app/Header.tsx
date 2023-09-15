e730ea500b78363878fa153f43e201204793b548
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-15 12:14:22 +0900

    header作成

---
+++ src/app/Header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className='py-5 px-10 border-b flex justify-between items-center'>
+        <div>
+          <h1 className="text-2xl font-extrabold">
+            <Link href="/">Next.js Blog</Link>
+          </h1>
+      </div>
+      <div>
+        <nav className='text-sm font-medium'>
+          <Link href="/article/new" className='bg-orange-300 px-3 py-3 rounded-md'>記事を書く</Link>
+        </nav>
+
+      </div>
+
+    </header>
+  );
+}
+
