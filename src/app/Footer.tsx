1e984656b55ec6bfe0bbf3770fcd229c3ba0a52e
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-16 06:03:22 +0900

    footer作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="py-2 px-4 border-t">
+      <small>©2023 anonymousアライさん lookUpTo @ShinCode</small>
+    </footer>
+  );
+};
