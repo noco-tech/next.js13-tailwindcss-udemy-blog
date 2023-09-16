1a3dded7aedefe60038e07d9d15c7c01f24ff639
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-16 16:58:50 +0900

    ArticleListのcss

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 export const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-90">
           <Image
             src="https://source.unsplash.com/collection/1345863/1000x500?sig=1"
             alt=""
@@ -14,17 +14,88 @@ export const ArticleList = () => {
             height={300}
           />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="#">Next勉強中</Link>
-          <p>By anonymous Published 2023/9/16</p>
-          <Link href="#">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est magnam expedita cum aliquam nulla voluptas alias repellendus iure illum, qui sunt. Consectetur, porro quasi. Error incidunt quae libero consequatur.
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
           </Link>
-          <Link href="#">
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published 2023/9/16</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est
+            magnam expedita cum aliquam nulla voluptas alias repellendus iure
+            illum, qui sunt. Consectetur, porro quasi. Error incidunt quae
+            libero consequatur.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
             つづきを読む
           </Link>
-
+        </div>
+      </article>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-90">
+          <Image
+            src="https://source.unsplash.com/collection/1345865/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published 2023/9/16</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est
+            magnam expedita cum aliquam nulla voluptas alias repellendus iure
+            illum, qui sunt. Consectetur, porro quasi. Error incidunt quae
+            libero consequatur.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            つづきを読む
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-90">
+          <Image
+            src="https://source.unsplash.com/collection/1345862/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published 2023/9/16</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est
+            magnam expedita cum aliquam nulla voluptas alias repellendus iure
+            illum, qui sunt. Consectetur, porro quasi. Error incidunt quae
+            libero consequatur.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            つづきを読む
+          </Link>
         </div>
       </article>
     </div>
