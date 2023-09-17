444123857f0b97f678a030019fa9e57bd3e93beb
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-17 19:28:18 +0900

    ArticleCardに切り分け,投稿文字数制限

---
+++ src/app/components/ArticleCard.tsx
@@ -0,0 +1,52 @@
+import { Article } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type ArticleCardProps = {
+  article: Article;
+};
+
+export const ArticleCard = ({ article }: ArticleCardProps) => {
+
+  return (
+    <article key={article.id} className="shadow my-4 flex flex-col">
+      <Link href={`articles/${article.id}`} className="hover:opacity-90">
+        <Image
+          src={`https://source.unsplash.com/collection/1345862/1000x500?sig=${article.id}`}
+          alt=""
+          width={1280}
+          height={300}
+        />
+      </Link>
+      <div className="bg-white flex flex-col justify-start p-6">
+        <Link
+          href={`articles/${article.id}`}
+          className="text-blue-700 pb-4 font-bold"
+        >
+          Technology
+        </Link>
+        <Link
+          href={`articles/${article.id}`}
+          className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+        >
+          {article.title}
+        </Link>
+        <p className="text-sm pb-3 text-slate-900">
+          Published {article.createdAt}
+        </p>
+        <Link href={`articles/${article.id}`} className="text-slate-900 pb-6">
+          {article.content.length > 70
+            ? article.content.substring(0, 70) + "..."
+            : article.content}
+        </Link>
+        <Link
+          href={`articles/${article.id}`}
+          className="text-pink-800 hover:text-black"
+        >
+          つづきを読む
+        </Link>
+      </div>
+    </article>
+  );
+};
--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,8 @@
-import { Article } from '@/types'
-import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
+
+import { Article } from '@/types'
+import { ArticleCard } from './ArticleCard'
+
 
 type ArticleListProps = {
   articles: Article[]
@@ -11,47 +12,9 @@ export const ArticleList = ({articles}: ArticleListProps) => {
 
   return (
     <div>
-      {articles.map((article) => (
-        <article key={article.id} className="shadow my-4 flex flex-col">
-          <Link href={`articles/${article.id}`} className="hover:opacity-90">
-            <Image
-              src={`https://source.unsplash.com/collection/1345862/1000x500?sig=${article.id}`}
-              alt=""
-              width={1280}
-              height={300}
-            />
-          </Link>
-          <div className="bg-white flex flex-col justify-start p-6">
-            <Link
-              href={`articles/${article.id}`}
-              className="text-blue-700 pb-4 font-bold"
-            >
-              Technology
-            </Link>
-            <Link
-              href={`articles/${article.id}`}
-              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-            >
-              {article.title}
-            </Link>
-            <p className="text-sm pb-3 text-slate-900">
-              Published {article.createdAt}
-            </p>
-            <Link
-              href={`articles/${article.id}`}
-              className="text-slate-900 pb-6"
-            >
-              {article.content}
-            </Link>
-            <Link
-              href={`articles/${article.id}`}
-              className="text-pink-800 hover:text-black"
-            >
-              つづきを読む
-            </Link>
-          </div>
-        </article>
-      ))}
+        {articles.map((article) => (
+        <ArticleCard article={article} key={article.id} />
+        ))}
     </div>
   );
 }
