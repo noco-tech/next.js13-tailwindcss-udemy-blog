f2fc68b81e2b080319877a79d4669b103339d916
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-17 19:58:04 +0900

    404作成

---
+++ src/app/articles/not-found.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center mt-20">
+      <div className="p-8 rounded-lg shadow-md text-center bg-slate-600">
+        <h1 className="text-2xl font-bold mb-4 text-gray-800">404</h1>
+        <p className="text-gray-400">ページが見つかりませんでした😢</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
