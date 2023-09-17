c904499b72211861b1d146176e2ce414744e8dc3
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-17 13:54:07 +0900

    errorページ作成

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,8 +1,14 @@
+
 import { Article } from "./types"
 
 //json-serverからssrで全記事取得
 export const getAllArticles = async (): Promise<Article[]> => {
-  const res = await fetch("http://localhost:3001/posts", { cache: "no-store"}) //・no-storeはSSRでキャッシュしない ・SSGはforce-cache　・ISRはnext: { revalidate: 3 }でキャッシュ
+  const res = await fetch("http://localhost:3001/posts", { cache: "no-store" }) //・no-storeはSSRでキャッシュしない ・SSGはforce-cache　・ISRはnext: { revalidate: 3 }でキャッシュ
+
+  if (!res.ok) {
+    throw new Error("エラーが発生したため記事を取得できませんでした。")
+  }
+
   const articles = await res.json()
   return articles
 }
