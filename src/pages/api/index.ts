be1f798d8edbbe72273fea10c66459e1a3bc5d1b
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-20 11:48:42 +0900

    src/pages/api/index.ts Supabaseと連携するAPI作成

---
+++ src/pages/api/index.ts
@@ -0,0 +1,17 @@
+// next.js公式ページAPI Routesより
+import { supabase } from "@/utils/supabaseClient";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { data: posts, error } = await supabase.from("posts").select("*");
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  return res.status(200).json(posts)
+}
